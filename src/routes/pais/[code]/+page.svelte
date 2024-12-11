047b0e9c9fed83de99b176ad69121a26c7a00cb6
Fernando Dodino <fernando.dodino@gmail.com>
2024-12-11 19:01:52 -0300

    Svelte plugin is a prettier wrapper!! so we need a prettierrc configuration file

--- src/routes/pais/[code]/+page.svelte
+++ src/routes/pais/[code]/+page.svelte
@@ -1,40 +1,40 @@
 <script lang="ts">
-	import './pais.css'
-	import { formatearEntero } from '$lib/utils'
-	import { goto } from '$app/navigation'
+  import './pais.css'
+  import { formatearEntero } from '$lib/utils'
+  import { goto } from '$app/navigation'
   import Bandera from '$lib/Bandera.svelte'
-	
-	let { data } = $props()
-	const { pais } = data
-	const volver = () => {
-	  goto('/')
-	}
+
+  let { data } = $props()
+  const { pais } = data
+  const volver = () => {
+    goto('/')
+  }
 </script>
 
 <div class="pais_page">
-	<div class="titulo">
-		<Bandera bandera={pais.bandera}/>
-		<h2>{pais.nombre}</h2>
-	</div>
-	<div class="pais_single">
-		<div><span>Capital: </span>{pais.capital}</div>
-		<div><span>Moneda:</span>{pais.moneda}</div>
-		<div>
-			<span>Superficie:</span>{formatearEntero(pais.area)} km<sup>2</sup>
-		</div>
-		<div class="poblacion">
-			<span>Población:</span>{formatearEntero(pais.poblacion)}
-			{#if pais.superpoblado()}
-				<img
-					alt="está superpoblado"
-					title="está superpoblado"
-					src="/crowd.png"
-					class="superpoblado"
-				/>
-			{/if}
-		</div>
-		<div><span>Código de país:</span>{pais.codigo}</div>
-		<div title={pais.timezone}><span>Qué hora es:</span>{pais.queHoraEs()}</div>
-	</div>
-	<button data-testid="volver" class="botonera" onclick={volver}>Volver</button>
+  <div class="titulo">
+    <Bandera bandera={pais.bandera} />
+    <h2>{pais.nombre}</h2>
+  </div>
+  <div class="pais_single">
+    <div><span>Capital: </span>{pais.capital}</div>
+    <div><span>Moneda:</span>{pais.moneda}</div>
+    <div>
+      <span>Superficie:</span>{formatearEntero(pais.area)} km<sup>2</sup>
+    </div>
+    <div class="poblacion">
+      <span>Población:</span>{formatearEntero(pais.poblacion)}
+      {#if pais.superpoblado()}
+        <img
+          alt="está superpoblado"
+          title="está superpoblado"
+          src="/crowd.png"
+          class="superpoblado"
+        />
+      {/if}
+    </div>
+    <div><span>Código de país:</span>{pais.codigo}</div>
+    <div title={pais.timezone}><span>Qué hora es:</span>{pais.queHoraEs()}</div>
+  </div>
+  <button data-testid="volver" class="botonera" onclick={volver}>Volver</button>
 </div>
